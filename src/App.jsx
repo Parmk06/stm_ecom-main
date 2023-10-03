14656f9ecc5080ed723d7a8b9203e656bdc29f3a
Parmk06 <parminderkaur723@gmail.com>
2023-10-03 13:34:17 -0400

    register file commited

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Register from "./pages/register";
 import './App.css';
+import Login from './pages/login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
