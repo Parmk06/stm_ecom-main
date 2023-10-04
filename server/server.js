b6a117a73b376824ab7ebc92395a8855fa740fba
Parmk06 <parminderkaur723@gmail.com>
2023-10-04 13:48:47 -0400

    database  connection fixed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import cookieParser from 'cookie-parser';
 const salt = 10;
 
@@ -19,20 +19,36 @@ const db = mysql.createConnection({
 })
 app.post('/register', (req, res)=>{
     const sql ="INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
-    bcrypt.hash(req.body.password.toString(),salt,(err, hash) => {
-        if(err) return res.json({Error: "Error for hashing password"});
-        const values = [
-            req.body.name,
-            req.body.email,
-            // req.body.password
-            hash
-        ]
-        db.query(sql, [values], (err, result) => {
-            if(err) return res.json({Error: "Inserting data Error in server"});
-            return res.json({Status: "Success"});
-        })
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, result) => {
+        if(err) return res.json({Error: "Inserting data Error in server"});
+        return res.json({Status: "Success"});
     })
+    
+    // bcrypt.hash(req.body.password.toString(),salt,(err, hash) => {
+    //     if(err) return res.json({Error: "Error for hashing password"});
+        
+    // })
 })
+
+//make login route by verifying email, password etc. if email, password mathes send user to home page if doesnot match send an error message saying email, password doesnt match...
+
+    // app.post('/login', (req, res)=>{
+    //     const sql ="INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
+    //     const values = [
+    //         req.body.name,
+    //         req.body.email,
+    //         req.body.password
+    //     ]
+    //     db.query(sql, [values], (err, result) => {
+    //         if(err) return res.json({Error: "Inserting data Error in server"});
+    //         return res.json({Status: "Success"});
+    //     })
+    
 
 app.listen(3000, () => {
     console.log("Running...");
